cb25fabbaffa1ce99eaf48d8c29530b083e3126d
frank <francismajere@gmail.com>
2022-11-01 20:50:25 +0800

    stage

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,7 +3,7 @@ import { component$, Slot } from '@builder.io/qwik';
 export default component$(() => {
   return (
   <div class="h-screen bg-gray-700">
-    <div class="flex flex-wrap justify-center justify-items-center items-center text-zinc-300 text-base">
+    <div class="flex flex-wrap justify-center justify-items-center items-center text-zinc-300 text-base py-3">
      <div class="flex-1">
       </div>
       <main class="sm:flex-none:w-full flex-initial w-132">
