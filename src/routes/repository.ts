0b032a3ebc7bfce8e8358246419a60108fa1ca7d
jingyu <francismajere@gmail.com>
2024-04-16 23:50:56 +0800

    feat: update deps

--- src/routes/repository.ts
+++ src/routes/repository.ts
@@ -1,12 +1,12 @@
-import type { IndexMenu } from "./entity";
+import type { IndexMenu } from "./entity"
 
 export const repo: IndexMenu[] = [
-	{
-		name: "A short journey into gRPC with Rust",
-		href: "/posts/grpcWithRust",
-		date: "2022-12-23",
-		type: "POST"
-	},
+	// {
+	// 	name: "A short journey into gRPC with Rust",
+	// 	href: "/posts/grpcWithRust",
+	// 	date: "2022-12-23",
+	// 	type: "POST"
+	// },
 	{
 		name: "Tests on gRPC timeout",
 		href: "https://github.com/washanhanzi/grpc-go-timeout",
@@ -19,10 +19,10 @@ export const repo: IndexMenu[] = [
 		date: "2022-07-22",
 		type: "EXTERNAL_LINK"
 	},
-	{
-		name: "Pragmatic gRPC 1 with Go",
-		href: "/posts/pragmaticgrpc1",
-		date: "2021-05-30",
-		type: "POST"
-	}
+	// {
+	// 	name: "Pragmatic gRPC 1 with Go",
+	// 	href: "/posts/pragmaticgrpc1",
+	// 	date: "2021-05-30",
+	// 	type: "POST"
+	// }
 ]
